c15fe0d8bd77ea4e2ba2f70360c4c32e3158145b
M-r-Nobody <46386316+M-r-Nobody@users.noreply.github.com>
2021-07-31 21:02:46 +0200

    Update scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,20 @@
+function car(model, silnik, biegi, skrzyniaBiegow, podgrzewaneSiedzenia, whatSoundDoesEngineMake, howManyMoreGearsWhouldYouLike) {
+    this.Model = model;
+    this.Silnik = silnik;
+    this.Biegi = biegi;
+    this.SkrzyniaBiegow = skrzyniaBiegow;
+    this.PodgrzewaneSiedzenia = podgrzewaneSiedzenia;
+    this.WhatSoundDoesEngineMake = whatSoundDoesEngineMake;
+    this.HowManyMoreGearsWhouldYouLike = howManyMoreGearsWhouldYouLike;
+}
+let BMW = new car("M3", "V12", 6, "manual", true, "", 0)
 
+
+
+function changeSound(sound) { BMW.WhatSoundDoesEngineMake = sound }
+function changeModel(model) { BMW.Model = model }
+function changeGearWish(gearWish) { BMW.HowManyMoreGearsWhouldYouLike = + gearWish }
+changeModel("E61")
+changeSound("Brrrrr")
+changeGearWish(12)
+console.log(BMW)
