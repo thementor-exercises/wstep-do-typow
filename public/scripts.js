f3f91eb4019a7dea3261169642c8b91df0bec060
Bartek <gorowski.bartlomiej@gmail.com>
2021-08-01 17:34:21 +0200

    Example solution

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,34 @@
 
+console.log(this);
+
+const Car = function (model) {
+  this.model = model;
+  this.engine = "V6";
+  this.gear = 6;
+  this.gearShaft = Symbol("Manual");
+  this.warmChairs = true;
+  console.log(this.gear);
+};
+
+Car.prototype = {
+  whatSoundDoesEngineMake: function() {
+    return "Brrr...";
+  },
+  howManyMoreGearsWhouldYouLike: function(gearToAdd) {
+    this.gear = this.gear + gearToAdd;
+  },
+  changeModel: function(model) {
+    this.model = model;
+  }
+};
+
+const audi = new Car("Audi");
+audi.howManyMoreGearsWhouldYouLike(1);
+const whatSoundDoesEngineMake = audi.whatSoundDoesEngineMake();
+
+console.log(audi);
+console.log(whatSoundDoesEngineMake);
+
+const bmw = new Car("BMW");
+
+console.log(bmw);
